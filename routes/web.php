4be01d78c16410884c18a864cf7a8b1531a8d95e
Orçun Otacıoğlu <otacioglu.orcun@gmail.com>
2016-12-27 18:02:24 +0200

    typo

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::resource('/sale', 'SalesController');
 Route::resource('/invoice', 'InvoiceController');
 Route::resource('/proforma', 'ProformaController');
 Route::get('/preview/{id}', 'InvoiceController@preview');
-Route::get('/generatedProforma/{$id}', 'ProformaController@generateProforma');
+Route::get('/proformaPreview/{$id}', 'ProformaController@generateProforma');
 Route::resource('/role', 'RolesController');
 Route::resource('/permission', 'PermissionsController');
 
