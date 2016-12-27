d8706b4c736e0da4d8b23366e96fefa45f8417d6
Orçun Otacıoğlu <otacioglu.orcun@gmail.com>
2016-12-27 18:58:28 +0200

    proforma css and view update

--- resources/views/dashboard/proforma/pdf.blade.php
+++ resources/views/dashboard/proforma/pdf.blade.php
@@ -5,8 +5,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    {{-- <link rel="stylesheet" type="text/css" href="{{ asset('css/print.css') }}"> --}}
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/proforma.css') }}">
 
 	<title>{{ $proforma->booking_id }} | Print Proforma</title>
 </head>
@@ -75,37 +74,5 @@
 			</table>
 		</div>
 	</div>
-	{{-- <div class="invoicePage">
-		
-		<div class="upperArea">
-			<div class="invoiceAdress">
-				<p>{{ $proforma->customer_name }}</p>
-				<p>{{ $proforma->customer_address }}</p>
-			</div>
-			<div class="invoiceDate">
-				<p>{{ $proforma->created_at }}</p>
-			</div>
-		</div>
-
-		<div class="midArea">
-			<div class="invoiceExplanation">
-
-				<div style="padding-left: 50px;">
-					@foreach ($items as $item)
-						<p>1 x {{ $item->area }} TICKET</p>
-					@endforeach
-				</div>
-				
-				<p style="margin-left: 424px; float: right; position: relative;">{{ $proforma->total }} EUR</p>
-			</div>
-		</div>
-
-		<div class="bottomArea">
-			<div class="invoiceTotal">
-				<p>{{ $proforma->total }} EUR</p>
-			</div>
-		</div>
-
-	</div> --}}
 </body>
 </html>
